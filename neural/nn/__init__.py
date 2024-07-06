9983ba71b7b451302acf3f0c994070c8ef23ef08
Ethan <luethan2025@outlook.com>
2024-07-05 20:05:18 -0700

    exported Dense, Sigmoid, ReLU, Tanh, and SoftmaxCrossEntropy

---
+++ neural/nn/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from .modules import Dense, Sigmoid, Tanh, ReLU
+from .modules import SoftmaxCrossEntropy
+
+__all__ = [
+  "Dense", "Sigmoid", "Tanh", "ReLU",
+  "SoftmaxCrossEntropy"
+]
