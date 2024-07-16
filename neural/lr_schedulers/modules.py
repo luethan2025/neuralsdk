0ffd42bfa3c3bf93f74e30d13058de362490cbfb
Ethan <luethan2025@outlook.com>
2024-07-15 22:17:02 -0700

    implemented MultiStepLR

--- neural/lr_schedulers/modules.py
+++ neural/lr_schedulers/modules.py
@@ -26,7 +26,7 @@ class StepLR(Scheduler):
   step_size : int
     Period of learning rate decay.
   gamma : float 
-    Learning rate decay parameter.
+    Learning rate decay parameter (defaults to 0.1).
   """
   def __init__(self, optimizer, step_size, gamma=0.1):
     self.step_size = step_size
@@ -39,3 +39,29 @@ class StepLR(Scheduler):
     self.last_epoch = self.last_epoch + 1
     if not (self.last_epoch % self.step_size):
       self.optimizer.adjust_lr(self.gamma)
+
+class MultiStepLR(Scheduler):
+  """Decay the learning rate by gamma when the number of epoch reaches a
+  specific milestone.
+
+  Parameters
+  ----------
+  optimizer : Optimizer
+    Optimizer policy.
+  milestones : int[]
+    List of epoch indices.
+  gamma : float 
+    Learning rate decay parameter (defaults to 0.1).
+  """
+  def __init__(self, optimizer, milestones, gamma=0.1):
+    self.milestones = milestones
+    self.gamma = gamma
+    self.last_epoch = 0
+    super().__init__(optimizer)
+
+  def step(self):
+    """Apply learning rate update policy."""
+    self.last_epoch = self.last_epoch + 1
+    if self.last_epoch in self.milestones:
+      self.optimizer.adjust_lr(self.gamma)
+    print(self.optimizer.lr)
