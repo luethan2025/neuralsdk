bef555b3a6788ad8e7d04436b213a9fb2d16030c
Ethan <luethan2025@outlook.com>
2024-07-16 17:03:31 -0700

    exported ChainedScheduler

--- neural/lr_schedulers/__init__.py
+++ neural/lr_schedulers/__init__.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
-from .modules import ConstantLR, StepLR, MultiStepLR
+from .modules import ConstantLR, StepLR, MultiStepLR, ChainedScheduler
 
-available_configurable_lr_schedulers = [StepLR, MultiStepLR]
+available_configurable_lr_schedulers = [StepLR, MultiStepLR, ChainedScheduler]
 __all__ = [
   "available_configuration_lr_schedulers",
-  "ConstantLR", "StepLR", "MultiStepLR"
+  "ConstantLR", "StepLR", "MultiStepLR", "ChainedScheduler"
 ]
