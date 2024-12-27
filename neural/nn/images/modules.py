7536c1f3d878cadcab2db77166ed6d141a0ccc99
Ethan Lu <luethan2025@outlook.com>
2024-12-27 01:28:36 -0800

    updated Flatten docstring

--- neural/nn/images/modules.py
+++ neural/nn/images/modules.py
@@ -3,12 +3,45 @@
 from ..base import Module
 
 class Flatten(Module):
-  """Flatten image into vector."""
+  """NumPy implementation of the Flatten Layer.
+
+  Notes:
+  ------
+  Flatten image into vector.
+  """
+  def __init__(self):
+    super().__init__()
+
   def forward(self, x):
-    """Forward propagation."""
+    """Forward propagation through Flatten.
+    
+    Parameters
+    ----------
+    x : np.array
+      Input for this layer.
+  
+    Returns
+    -------
+    np.array
+      Output of this layer.
+    """
     self.shape = x.shape
     return x.reshape(x.shape[0], -1)
 
   def backward(self, grad):
-    """Backward propagation."""
+    """
+    Backward propogation for Flatten.
+
+    Parameters
+    ----------
+    grad : np.array
+      Gradient (Loss w.r.t. data) flowing backwards from the next layer,
+      dL/dx_k. Should have dimensions (batch, dim).
+
+    Returns
+    -------
+    np.array
+      Gradients for the inputs to this layer, dL/dx_{k-1}. Should
+      have dimensions (batch, dim).
+    """
     return grad.reshape(self.shape)
